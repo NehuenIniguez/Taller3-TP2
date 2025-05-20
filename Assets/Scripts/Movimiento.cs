71a781db061a8e862e7d06d01b98293e70e0233b
NehuenIniguez <164099712+NehuenIniguez@users.noreply.github.com>
2025-05-19 21:22:11 -0300

    Feature/movimiento

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -2,15 +2,53 @@ using UnityEngine;
 
 public class Movimiento : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Vector2 startTouchPosition;
+    private Vector2 endTouchPosition;
+    public float movimientoDistancia = 1500f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Al tocar la pantalla
+            if (touch.phase == TouchPhase.Began)
+            {
+                startTouchPosition = touch.position;
+            }
+
+            // Al levantar el dedo
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                endTouchPosition = touch.position;
+            }
+        }
+        Vector2 swipe = endTouchPosition - startTouchPosition;
+
+        swipe.Normalize();
+
+        if (Vector2.Dot(swipe, Vector2.up) > 0.7f)
+            {
+            Debug.Log("Swipe hacia arriba");
+            transform.position += Vector3.up * movimientoDistancia * Time.deltaTime  ;
+            }
+        else if (Vector2.Dot(swipe, Vector2.down) > 0.7f)
+            {
+            Debug.Log("Swipe hacia abajo");
+            transform.position += Vector3.down * movimientoDistancia * Time.deltaTime ;
+            }
+        else if (Vector2.Dot(swipe, Vector2.left) > 0.7f)
+            {
+            Debug.Log("Swipe a la izquierda");
+            transform.position += Vector3.left * movimientoDistancia * Time.deltaTime ;
+            }
+        else if (Vector2.Dot(swipe, Vector2.right) > 0.7f)
+            {
+            Debug.Log("Swipe a la derecha");
+            transform.position += Vector3.right * movimientoDistancia * Time.deltaTime ;
+            }
     }
+
+    
 }
