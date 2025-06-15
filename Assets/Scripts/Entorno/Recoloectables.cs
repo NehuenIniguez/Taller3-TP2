ee6afd40c4dbd7745fed0c8b6cb25be58ce0144a
NehuenIniguez <164099712+NehuenIniguez@users.noreply.github.com>
2025-06-15 19:24:17 -0300

    proceso de implementar ia

--- Assets/Scripts/Entorno/Recoloectables.cs
+++ Assets/Scripts/Entorno/Recoloectables.cs
@@ -4,12 +4,20 @@ public class Recoloectables : MonoBehaviour
 {
     public int puntos = 1;
     [SerializeField] private Puntaje puntaje;
+    private AudioSource audioSource;
+    public AudioClip sonido;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Personaje"))
         {
+            
             puntaje.SumaPuntos(puntos);
+            audioSource.PlayOneShot(sonido);
             Destroy(gameObject);
             
         }
